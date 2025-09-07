4e7b2215db7586de9f6bbae4bf644ef52c902748
Erik Demchak <demcsakerik@gmail.com>
2025-09-07 14:16:37 +0200

    remove packages

---
+++ shared/auth.ts
@@ -0,0 +1,6 @@
+import { z } from 'zod';
+
+export const LoginInput = z.object({});
+export const LoginOutput = z.object({});
+export const UserPublic = z.object({});
+export const ErrorEnvelope = z.object({});
