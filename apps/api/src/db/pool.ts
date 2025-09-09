1abcc62863d87678f86a29cd3243b53f26c03d15
Erik Demchak <demcsakerik@gmail.com>
2025-09-09 21:45:56 +0200

    remove

--- apps/api/src/db/pool.ts
+++
@@ -1,9 +0,0 @@
-import '../env-setup';
-import { Pool } from 'pg';
-import { getDbConfig } from '../config/env.util';
-
-// Responsible for initializing and exporting a singleton pg.Pool using DATABASE_URL and SSL config
-
-const dbConfig = getDbConfig();
-
-export const pool = new Pool(dbConfig);
--- apps/web/src/App.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { LoginInput, userSchema } from '@acme/shared';
-
-function App() {
-  return <h1>Hello from Vite + React!</h1>;
-}
-
-export default App;
